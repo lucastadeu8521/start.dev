2469e4ffaf5bfc7d50cb0fa86936c94277503ca0
Lucas Tadeu Nascimento da Silva <1241306653@acd.grupotiradentes.com>
2025-04-28 20:43:20 -0300

    esqueleto da aplicacao

---
+++ api/src/main/java/dev/start/api/UserController.java
@@ -0,0 +1,26 @@
+package dev.start.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+    @PostMapping
+    public void cadastrar(){
+
+    }
+    @GetMapping
+    public void listar(){
+
+    }
+    @DeleteMapping
+    public void delete(){
+
+    }
+    @PutMapping
+    public void atualizar(){
+
+    }
+}
---
+++ api/src/main/resources/database/V1__create-table-usuario.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuario (
+usuarioID INT AUTO_INCREMENT PRIMARY KEY,,
+data_criacao DATE,
+usuario VARCHAR(100),
+senha VARCHAR(100),
+nome_usuario VARCHAR(100),
+sobrenome_usuario VARCHAR(100)
+);
